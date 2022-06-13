1c457766bca731c4b553df9a0174601e68be3a0a
sinijeong <sinijeong.only@gmail.com>
2022-06-13 15:56:35 +0900

    feat(auth-page): restrict access to authenticated users

--- webapp/src/pages/auth/AuthPage.tsx
+++ webapp/src/pages/auth/AuthPage.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { useMatch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useMatch, useNavigate } from 'react-router-dom';
 import { LoginForm, SignUpForm } from '../../containers/auth';
+import { useLogged } from '../../hooks/useLogged';
 
 function AuthPage() {
   const isLoginPage = useMatch('/login');
+  const logged = useLogged();
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    if (logged) {
+      navigation('/', { replace: true });
+    }
+  }, [logged]);
 
   return <>{isLoginPage ? <LoginForm /> : <SignUpForm />}</>;
 }
